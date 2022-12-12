af52b094911d508d407fe948d391b4ebb0732de6
Danil26m <111749790+Danil26m@users.noreply.github.com>
2022-12-12 22:36:46 +0200

    Update Contacts.jsx

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -37,7 +37,6 @@ filtered=()=>{
 }
 delete=(even)=>{
     const{contacts} = this.props.cont;
-    this.setStatee({contacts : contacts.filter((cont)=> cont.id !== even.target.id)})
     this.setState({contacts : contacts.filter((cont)=> cont.id !== even.target.id)})
     contacts.splice(even.target.id);
 }
